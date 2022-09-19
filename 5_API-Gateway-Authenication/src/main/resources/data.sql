f204e0e35d4c6031f360600a3f79209185078b99
rajkumar <rajnani512@gmail.com>
2022-09-19 10:51:31 +0530

    Initial commit

---
+++ 5_API-Gateway-Authenication/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into user values('admin','admin',1);
+insert into user values('vidhi','vidhi',2);
+insert into user values('gautam','gautam',3);
+insert into user values('lucky','lucky',4);
+insert into user values('vikas','vikas',5);
+insert into user values('bhuvnesh','bhuvnesh',6);
